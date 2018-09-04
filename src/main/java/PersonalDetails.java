ba9b571fede4b131865c53f8dc0f8c200c83895b
Robert Buican <robert.buican@gmail.com>
2018-09-04 22:41:37 +0100

    Implement json parser

---
+++ src/main/java/PersonalDetails.java
@@ -0,0 +1,16 @@
+public class PersonalDetails {
+    private String firstName;
+    private String secondName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getSecondName() {
+        return secondName;
+    }
+    public void setCity(String secondName) {
+        this.secondName = secondName; }
+}
---
+++ src/main/java/main.java
@@ -0,0 +1,18 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class main {
+    public static void main(String[] args) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        PersonalDetails person = null;
+
+        try {
+            InputStream json = new FileInputStream("person.json");
+            PersonalDetails person = objectMapper.readValue(json, PersonalDetails.class);
+        } catch (IOException exception) {
+    }
+        System.out.println(person);
+}
